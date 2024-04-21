dc61d21ea144d69245820b118803a0a4e642ed5d
Duy Tran <duytran_d@yahoo.com>
2024-04-21 17:02:22 -0500

    Update dependencies and add new files

---
+++ client/src/baseUrl.js
@@ -0,0 +1,5 @@
+// const baseUrl = "https://duywebapp2.azurewebsites.net";
+const baseUrl = "http://localhost:3001";
+//just test a
+
+export default baseUrl;
--- server/index.js
+++ server/index.js
@@ -10,10 +10,6 @@ const db = require("./models");
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-const commentsRouter = require("./routes/Comments");
-app.use("/comments", commentsRouter);
-const usersRouter = require("./routes/Users");
-app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
