c5c3444fd9bdd28716c5304d59803c5c9b8793d9
Duy Tran <duytran_d@yahoo.com>
2024-04-21 16:48:30 -0500

    Update commit messages for episodes 3-9

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
+import baseUrl from "./baseUrl";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost";
-import Post from "./pages/Post";
-import Registration from "./pages/Registration";
-import Login from "./pages/Login";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/posts`).then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <Router>
-        <div className="navbar">
-          <Link to="/"> Home Page</Link>
-          <Link to="/createpost"> Create A Post</Link>
-          <Link to="/login"> Login</Link>
-          <Link to="/registration"> Registration</Link>
-        </div>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/createpost" exact component={CreatePost} />
-          <Route path="/post/:id" exact component={Post} />
-          <Route path="/registration" exact component={Registration} />
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </Router>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div key={key} className="post">
+            <div className="title"> {value.title} </div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
